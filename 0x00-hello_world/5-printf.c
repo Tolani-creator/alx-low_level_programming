6e309f96f55ea1a2523189c16019cd836f960a11
Giwa Mubarak <giwaomogiwa@gmail.com>
2022-09-09 11:24:31 -0700

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
- *
- * Return: always 0
+*main - Entry point
+*
+*Return: Always 0
 */
-int main(void)
+int main (void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+printf(" with proper grammar, but the outcome is a piece of art, \n");
+
+return (0);
 }
