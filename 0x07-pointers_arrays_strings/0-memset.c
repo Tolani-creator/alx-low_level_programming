99fc1a763c0bd516f169d8f95f5245fcba46a631
Giwa Mubarak <giwaomogiwa@gmail.com>
2022-09-27 16:19:08 -0700

    1. memcpy

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,66 +1,18 @@
-main.h.txt
-
-Who has access
-
-A
-
-System properties
-
-Type
-
-Text
-
-Size
-
-511 bytes
-
-Storage used
-
-511 bytes
-
-Location
-
-0x07. C - Even more pointers, arrays and strings
-
-Owner
-
-Antony Bahati
-
-Modified
-
-26 Sept 2022 by Antony Bahati
-
-Opened
-
-00:07 by me
-
-Created
-
-27 Sept 2022
-
-No description
-
-Viewers can download
-
-#include "main.h"
-
-
-
 /**
 
- * _memset - fills a memory block with a constant byte
+* _memset - fills a memory block with a constant byte
 
- * @s: address to memory block
+* @s: address to memory block
 
- * @b: char to be used
+* @b: char to be used
 
- * @n: number of bytes to be used
+* @n: number of bytes to be used
 
- *
+*
 
- * Return: pointer to the memory block
+* Return: pointer to the memory block
 
- */
+*/
 
 
 
@@ -68,16 +20,14 @@ char *_memset(char *s, char b, unsigned int n)
 
 {
 
-	while (n)
+while (n)
 
-	{
+s[n - 1] = b;
 
-		s[n - 1] = b;
+n--;
 
-		n--;
+}
 
-	}
-
-	return (s);
+return (s);
 
 }
