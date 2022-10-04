f12ff2e7b76fe0ee0d17d107eb4efb064ea60e6e
Giwa Mubarak <giwaomogiwa@gmail.com>
2022-10-04 13:11:09 -0700

    creating array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,7 +13,7 @@ char *create_array(unsigned int size, char c)
 {
 char *array;
 unsigned int index;
- 
+
 if (size == 0)
 return (NULL);
 array = malloc(sizeof(char) * size);
@@ -24,5 +24,5 @@ return (NULL);
 for (index = 0; index < size; index++)
 array[index] = c;
 
-return(array);
+return (array);
 }
