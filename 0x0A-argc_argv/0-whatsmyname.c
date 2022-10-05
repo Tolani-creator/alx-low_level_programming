36098249c0b2d5c7b5f862c17928829f7cbd8225
Giwa Mubarak <giwaomogiwa@gmail.com>
2022-10-05 04:04:17 -0700

    TASK 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - prints the name of the program
+* @argc: number of arguments passed of the function
+* @argv: argument vector of pointers to string
+* Return: always 0
+*/
+int main(int argc __attribute__((unusded)), char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,4 @@
+0. It ain't what they call you, it's what you answer to 
+1. Silence is argument carried out by other means 
+3. Neither irony nor sarcasm is argument 
+4. To infinity and beyond 
